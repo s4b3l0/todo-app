621ca7e9783a4adf5a4312e89474fcb806e813bd
sabelo <sabelo.nkosi@rocketmail.com>
2023-04-18 15:12:27 +0200

    App starts up

--- todo-service/src/main/java/com/todo/app/ProcessTodoDelegate.java
+++ todo-service/src/main/java/com/todo/app/ProcessTodoDelegate.java
@@ -1,19 +1,8 @@
 package com.todo.app;
 
-import com.todo.app.entity.TodoItem;
-import lombok.experimental.Delegate;
-import org.camunda.bpm.engine.delegate.DelegateExecution;
-import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ProcessTodoDelegate implements JavaDelegate {
+public class ProcessTodoDelegate  {
 
-    @Override
-    public void execute(DelegateExecution execution) throws Exception {
-        TodoItem todoItem = new TodoItem();
-        todoItem.setTitle("Hello");
-        execution.setVariable("name", false);
-        execution.setVariable("todo", todoItem);
-    }
 }
--- todo-service/src/main/java/com/todo/app/rest/impl/com/todo/app/rest/TodoItemController.java
+++ todo-service/src/main/java/com/todo/app/rest/impl/com/todo/app/rest/TodoItemController.java
@@ -1,13 +1,8 @@
 package com.todo.app.rest.impl.com.todo.app.rest;
 
-import com.todo.app.ProcessTodoDelegate;
 import com.todo.app.entity.TodoItem;
 import com.todo.app.rest.impl.GenericController;
 import com.todo.app.service.impl.com.todo.app.service.TodoItemService;
-import org.camunda.bpm.engine.ProcessEngine;
-import org.camunda.bpm.engine.ProcessEngines;
-import org.camunda.bpm.engine.RuntimeService;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,12 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/todo")
 public class TodoItemController extends GenericController<TodoItemService, TodoItem> {
     private TodoItemService service;
-    private RuntimeService runtimeService;
 
-    public TodoItemController(TodoItemService service, RuntimeService runtimeService) {
+    public TodoItemController(TodoItemService service) {
         super(service);
         this.service = service;
-        this.runtimeService = runtimeService;
     }
 
 }
